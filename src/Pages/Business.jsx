81008fcf944e0bbacd18d0882bf18e8b73c50fe0
Firda Rosiana T <firdaroziana10@gmail.com>
2024-10-26 18:43:30 +0700

    update business

--- src/Pages/Business.jsx
+++ src/Pages/Business.jsx
@@ -10,31 +10,31 @@ const Business = () => {
         <div className="container max-w-screen-xl mx-auto px-4">
           <Nav />
           <div className="mx-auto mb-28 flex">
-            <div className="grid gap-4 justify-center w-full max-w-screen-xl px-24">
+            <div className="grid gap-4 justify-center w-full max-w-screen-xl lg:px-10">
               <div className="flex flex-col items-center">
-                <img className="h-auto max-w-sm rounded-lg" src="/potreed.jpg" alt="Main Image" />
-                <p className="mt-4 mb-8 text-lg text-center font-normal text-gray-800 lg:text-xl sm:px-16 lg:px-40">
-                  I have a team business called <span className="font-bold">POTREED</span>, and we are dedicated to capturing moments that matter. From graduation and events to family portraits and creative projects, we aim to create
+                <img className="h-auto max-w-xs rounded-lg w-50" src="/potreed.jpg" alt="Main Image" />
+                <p className="mt-4 mb-8 text-lg text-center font-normal text-gray-800 lg:text-2xl sm:px-12 lg:px-36">
+                  I have a team business called <span className="font-semibold">POTREED</span>, and we are dedicated to capturing moments that matter. From graduation and events to family portraits and creative projects, we aim to create
                   stunning images that reflect your personal story
                 </p>
               </div>
 
               <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4 px-5">
                 <div>
-                  <img className="h-auto w-full rounded-lg" src="/gallery/graduate/1.png" alt="Gallery Image 1" />
+                  <img className="h-auto w-full rounded-lg transition-all duration-300 hover:scale-125 shadow-md" src="/gallery/graduate/1.png" alt="Gallery Image 1" />
                 </div>
                 <div>
-                  <img className="h-auto w-full rounded-lg" src="/gallery/graduate/1.png" alt="Gallery Image 2" />
+                  <img className="h-auto w-full rounded-lg transition-all duration-300 hover:scale-125 shadow-md" src="/graduate-2.png" alt="Gallery Image 2" />
                 </div>
                 <div>
-                  <img className="h-auto w-full rounded-lg" src="/gallery/graduate/1.png" alt="Gallery Image 3" />
+                  <img className="h-auto w-full rounded-lg transition-all duration-300 hover:scale-125 shadow-md" src="/graduate-1.png" alt="Gallery Image 3" />
                 </div>
                 <div>
-                  <img className="h-auto w-full rounded-lg" src="/gallery/graduate/1.png" alt="Gallery Image 4" />
+                  <img className="h-auto w-full rounded-lg transition-all duration-300 hover:scale-125 shadow-md" src="/graduate-3.png" alt="Gallery Image 4" />
                 </div>
               </div>
 
-              <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 my-12">
+              <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 mt-12 mb-16 lg:mb-20">
                 <Button outline gradientDuoTone="greenToBlue" className="animate-bounce ">
                   <div className="flex text-2xl justify-center items-center">
                     EXPLORE PACKAGES HERE
@@ -44,22 +44,25 @@ const Business = () => {
                   </div>
                 </Button>
               </div>
+              <h2 class="text-4xl font-semibold text-gray-900 text-center mb-6">Service FAQ</h2>
               <Accordion collapseAll>
                 <Accordion.Panel>
-                  <Accordion.Title>What is Flowbite?</Accordion.Title>
+                  <h1></h1>
+                  <Accordion.Title className="text-lg">Paket apa saja yang tersedia?</Accordion.Title>
                   <Accordion.Content>
-                    <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
+                    <p className="mb-2 text-gray-500 dark:text-gray-400">
+                      Kami menawarkan berbagai paket fotografi, termasuk paket untuk acara pribadi, pre-wedding, dan sesi foto keluarga. Setiap paket dapat disesuaikan sesuai kebutuhan klien Available untuk private photoshoot
+                    </p>
                     <p className="text-gray-500 dark:text-gray-400">
-                      Check out this guide to learn how to&nbsp;
-                      <a href="https://flowbite.com/docs/getting-started/introduction/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                        get started&nbsp;
+                      Check out this guide to see
+                      <a href="https://flowbite.com/docs/getting-started/introduction/" className="text-cyan-600 hover:underline dark:text-cyan-500 ml-1">
+                        the package&nbsp;
                       </a>
-                      and start developing websites even faster with components on top of Tailwind CSS.
                     </p>
                   </Accordion.Content>
                 </Accordion.Panel>
                 <Accordion.Panel>
-                  <Accordion.Title>Is there a Figma file available?</Accordion.Title>
+                  <Accordion.Title className="text-lg">Is there a Figma file available?</Accordion.Title>
                   <Accordion.Content>
                     <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
                     <p className="text-gray-500 dark:text-gray-400">
@@ -72,16 +75,10 @@ const Business = () => {
                   </Accordion.Content>
                 </Accordion.Panel>
                 <Accordion.Panel>
-                  <Accordion.Title>What are the differences between Flowbite and Tailwind UI?</Accordion.Title>
+                  <Accordion.Title className="text-lg">Apakah untuk pemotretan terbatas beberapa shoot? Berapa rata-rata hasil photoshoot yang akan saya dapat?</Accordion.Title>
                   <Accordion.Content>
-                    <p className="mb-2 text-gray-500 dark:text-gray-400">
-                      The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
-                      components, whereas Tailwind UI offers sections of pages.
-                    </p>
-                    <p className="mb-2 text-gray-500 dark:text-gray-400">
-                      However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.
-                    </p>
-                    <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                    <p className="mb-2 text-gray-500 dark:text-gray-400">Jumlah gambar yang diambil tidak terbatas, namun rata-rata klien kami menerima antara 50-100 gambar hasil editan tergantung paket yang dipilih.</p>
+
                     <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
                       <li>
                         <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
